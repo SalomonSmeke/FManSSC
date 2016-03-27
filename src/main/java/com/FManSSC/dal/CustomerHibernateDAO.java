dc3427f10547718b8189e8d6dc551725d454ba85
Salomon Smeke <ssmeke@luc.edu>
2016-03-27 13:11:12 -0500

    starting dao

--- src/main/java/com/FManSSC/dal/CustomerHibernateDAO.java
+++ src/main/java/com/FManSSC/dal/CustomerHibernateDAO.java
@@ -1,5 +1,26 @@
 package com.FManSSC.dal;
 
+import org.hibernate.Session;
+
+import com.FManSSC.model.customer.Customer;
+
 public class CustomerHibernateDAO {
 
+	public void addCustomer(Customer cust) {
+		//TODO: Verify
+		System.out.println("*************** Adding customer information in DB with ID ...  " + cust.getId());
+		Session session = HibernatePGSQLHelper.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		session.save(cust);
+		session.getTransaction().commit();
+	}
+	
+	public void deleteCustomer(Customer cust) {
+		System.out.println("*************** Deleteing customer information in DB with ID ...  " + cust.getId());
+		Session session = HibernatePGSQLHelper.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		session.delete(cust);
+		session.getTransaction().commit();
+	}
+	
 }
--- src/main/java/com/FManSSC/model/customer/Customer.java
+++ src/main/java/com/FManSSC/model/customer/Customer.java
@@ -5,19 +5,19 @@ import com.FManSSC.model.backbones.Person;
 
 public class Customer extends Person implements _Customer {
 
-	private Address address;
+	private Address custAddress;
 
 	public Address getAddress() {
-		return address;
+		return custAddress;
 	}
 	public void setAddress(Address address) {
-		this.address = address;
+		this.custAddress = address;
 	}
 
 	@Override
 	public boolean verify(){
 		if (!super.verify()) return false;
-		if (!address.verify()) return false;
+		if (!custAddress.verify()) return false;
 		return true;
 	}
 

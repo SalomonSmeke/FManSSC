fdb1a3385328c8d45e320c1fe92b25c3253322d7
Salomon Smeke <ssmeke@luc.edu>
2016-03-26 21:09:12 -0500

    heroku config

--- src/main/java/com/FManSSC/dal/HibernatePGSQLHelper.java
+++ src/main/java/com/FManSSC/dal/HibernatePGSQLHelper.java
@@ -1,5 +1,7 @@
 package com.FManSSC.dal;
 
+import java.net.URI;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -9,7 +11,16 @@ public class HibernatePGSQLHelper {
 	static {
 		try {
 			// Create the SessionFactory from hibernate.cfg.xml
-			sessionFactory = new Configuration().configure().buildSessionFactory();
+			Configuration config = new Configuration().configure();
+			URI database = new URI(System.getenv("DATABASE_URL"));
+			String [] user = database.getUserInfo().split(":");
+			String builtUrl = "jdbc:postgresql://" + database.getHost() + ":" + database.getPort() + database.getPath();
+			
+			config.setProperty("hibernate.connection.url", builtUrl);
+			config.setProperty("hibernate.connection.username", user[0]);
+			config.setProperty("hibernate.connection.password", user[1]);
+			
+			sessionFactory = config.buildSessionFactory();
 			System.out.println("*************** Session Factory instantiated ..................");
 		} catch (Throwable ex) {
 			// Make sure you log the exception, as it might be swallowed

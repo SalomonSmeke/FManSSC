ae9d008ffb9bea73b1a995cf245d119625f197ae
Salomon Smeke <ssmeke@luc.edu>
2016-03-27 14:25:01 -0500

    servlet config

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 
 import javax.servlet.http.*;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 public class Main extends HttpServlet {
 	
@@ -11,8 +13,13 @@ public class Main extends HttpServlet {
 		
 	}
 	
-	public static void main(String... args)  {
+	public static void main(String... args) throws Exception  {
 		Server server = new Server(Integer.valueOf(System.getenv("PORT")));
-		
+		ServletContextHandler ctx = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		ctx.setContextPath("/");
+		server.setHandler(ctx);
+		ctx.addServlet(new ServletHolder(new Main()), "/*");
+		server.start();
+		server.join();
 	}
 }

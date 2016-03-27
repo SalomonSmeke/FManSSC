ffddd0bc89608e5be04fbdd923d1f50a84de8399
Salomon Smeke <ssmeke@luc.edu>
2016-03-27 14:14:43 -0500

    servlet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,18 @@
+import java.io.IOException;
 
+import javax.servlet.http.*;
+import org.eclipse.jetty.server.Server;
 
-public class Main {
-  public static void main(String... args) throws Exception {
-   
-  }
+public class Main extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+		response.getWriter().println("hi");;
+		
+	}
+	
+	public static void main(String... args)  {
+		Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+		
+	}
 }

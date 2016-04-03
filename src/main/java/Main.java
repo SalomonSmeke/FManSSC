9ad3f3dacc92fbcb135f77b57a08289aeb443019
Salomon Smeke <ssmeke@luc.edu>
2016-04-03 14:47:40 -0500

    POJO tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,9 +10,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.FManSSC.dal.*;
 import com.FManSSC.model.backbones.Address;
+import com.FManSSC.model.backbones._Synopsis;
+import com.FManSSC.model.backbones._Verifiable;
 import com.FManSSC.model.customer.Customer;
 
+import Fixtures.Addresses;
 import Fixtures.Customers;
+import Fixtures.Facilities;
+import Fixtures.Owners;
+import Fixtures.Phones;
+import Fixtures.Reservations;
+import Fixtures.Rooms;
+import Fixtures.Tickets;
+import Fixtures.Times;
 
 @SuppressWarnings("serial")
 public class Main extends HttpServlet {
@@ -54,9 +64,44 @@ public class Main extends HttpServlet {
 		server.join();
 	}
 	
-	protected void demoPOJO(PrintWriter out, String string){
-		
-		ApplicationContext context = new ClassPathXmlApplicationContext("META-INF/spring/app-context.xml");
+	protected void demoPOJO(PrintWriter out, String PREFIX){
+		out.println(PREFIX + "POJO Address");
+		out.println(objectSynopsis(PREFIX,Addresses.add0));
+		out.println(objectVerification(PREFIX,Addresses.add0));
+		out.println();
+		out.println(PREFIX + "POJO Customer");
+		out.println(objectSynopsis(PREFIX,Customers.cust0));
+		out.println(objectVerification(PREFIX,Customers.cust0));
+		out.println();
+		out.println(PREFIX + "POJO Facility");
+		out.println(objectSynopsis(PREFIX,Facilities.fa0));
+		out.println(objectVerification(PREFIX,Facilities.fa0));
+		out.println();
+		out.println(PREFIX + "POJO Owner");
+		out.println(objectSynopsis(PREFIX,Owners.own0));
+		out.println(objectVerification(PREFIX,Owners.own0));
+		out.println();
+		out.println(PREFIX + "POJO Phone");
+		out.println(objectVerification(PREFIX,Phones.ph0));
+		out.println();
+		out.println(PREFIX + "POJO Reservation");
+		out.println(objectSynopsis(PREFIX,Reservations.re0));
+		out.println(objectVerification(PREFIX,Reservations.re0));
+		out.println();
+		out.println(PREFIX + "POJO Room");
+		out.println(objectSynopsis(PREFIX,Rooms.ro0));
+		out.println(objectVerification(PREFIX,Rooms.ro0));
+		out.println();
+		out.println(PREFIX + "POJO Ticket");
+		out.println(objectSynopsis(PREFIX,Tickets.ti0));
+		out.println(objectVerification(PREFIX,Tickets.ti0));
+		out.println();
+		out.println(PREFIX + "POJO Time");
+		out.println(objectVerification(PREFIX,Times.ti0));
+		out.println();
+	}
+	
+	protected void demoBeans(PrintWriter out, String string, ApplicationContext context){
 		Customer cust = Customers.cust0;
 		cust.setAddress((Address)context.getBean("addressFixture"));
 		boolean v = cust.verify();
@@ -69,15 +114,15 @@ public class Main extends HttpServlet {
 		out.println("looks good jim!");
 	}
 	
-	protected void demoBeans(PrintWriter out, String string, ApplicationContext context){
-		
-	}
-	
 	protected void demoDB(PrintWriter out, String string, ApplicationContext context){
 		
 	}
 	
-	protected String objectResult(){
-		return "";
+	protected String objectSynopsis(String PREFIX, _Synopsis obj){
+		return PREFIX + "Synopsis:"+ obj.synopsis();
+	}
+	
+	protected String objectVerification(String PREFIX, _Verifiable obj){
+		return PREFIX + "Verification Result:" + obj.verify();
 	}
 }
